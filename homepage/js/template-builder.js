f23dafdcfc11a5ccef4df499f5aac3d6b8b8cb2a
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2023-05-31 00:21:24 +0200

    added technologies

--- homepage/js/template-builder.js
+++ homepage/js/template-builder.js
@@ -18,14 +18,15 @@ async function parseProjects() {
         .then(response => response.json())
         .then(data => {
             data.forEach(project => {
-                let title, description, website, github;
+                let title, description, technologies, website, github;
 
                 if(project.title) title = project.title; else title = 'No title';
                 if(project.description) description = project.description; else description = 'No description';
+                if(project.technologies) technologies = project.technologies; else technologies = 'No technologies listed';
                 if(project.website) website = project.website; else website = 'No link';
                 if(project.github) github = project.github; else github = 'No link';
 
-                projectList.push({ title, description, website, github });
+                projectList.push({ title, description, technologies, website, github });
             });
         });
 }
@@ -46,6 +47,9 @@ function buildProjects() {
 
         if(project.title) projectHTML = projectHTML.replaceAll('{{title}}', project.title);
         if(project.description) projectHTML = projectHTML.replaceAll('{{description}}', project.description);
+        if(project.technologies) 
+            if(Array.isArray(project.technologies)) projectHTML = projectHTML.replaceAll('{{technologies}}', project.technologies.join(', '));
+            else projectHTML = projectHTML.replaceAll('{{technologies}}', project.technologies);
         if(project.website) projectHTML = projectHTML.replaceAll('{{website}}', project.website);
         if(project.github) projectHTML = projectHTML.replaceAll('{{github}}', project.github);
 
